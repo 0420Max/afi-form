b19fa1abe071a156c2669bf544d483ac6ec8ea8b
0420Max <maximebelzile@gmail.com>
2026-04-14 21:17:24 -0400

    Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import AFIForm from './AFIForm'
+
+export default function App() {
+  return (
+    <div style={{ minHeight: '100vh', background: '#f8f7f4' }}>
+      <AFIForm />
+    </div>
+  )
+}
